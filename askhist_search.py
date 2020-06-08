69c632fe038bb3469d2bb1d4aa149c74dd98752a
james <tmb18134@uni.strath.ac.uk>
2020-06-08 15:10:12 +0100

    Implemented CLI, now accepts only mandatory and only optional

--- askhist_search.py
+++ askhist_search.py
@@ -1,3 +1,5 @@
+import sys
+
 import plac
 import praw
 from praw.models import Subreddit
@@ -20,13 +22,19 @@ def build_searchstring(mandatory, optional):
     """Take in two lists of keywords and output a reddit-search string
     Mandatory keywords are those which must all occur in the post,
     whereas only one of the optional keywords must appear"""
-    search_string = "({})"
-    necessary = " AND ".join(mandatory)
+
+    # I hate how this looks, but I don't see a much better way of doing it
+    # DRY is going to get violated no matter what you do
+    mandatory_words = "({})"
+    if mandatory is not None:
+        mandatory_words = mandatory_words.format(" AND ".join(mandatory))
+    optional_words = "({})"
     if optional is not None:
-        search_string += " AND ({})"
-        options = " OR ".join(optional)
-        return search_string.format(necessary, options)
-    return search_string.format(necessary)
+        optional_words = optional_words.format(" OR ".join(optional))
+        if mandatory is not None:
+            return "{} AND {}".format(mandatory_words, optional_words)
+        return optional_words
+    return mandatory_words
 
 
 def search_with(askhistorians: Subreddit, targets, optional=None, n=10):
@@ -50,14 +58,18 @@ def search_with(askhistorians: Subreddit, targets, optional=None, n=10):
 
 
 @plac.annotations(
-    mandatory=("Keywords which a post must have", "positional", None, str.split),
+    mandatory=("Keywords which a post must have", "option", "m", str.split),
     optional=("Keywords which a post must have at least one of", "option", "p", str.split),
-    n=("The number of results to search", "option", "n", int))
-def main(mandatory, optional, n):
+    n=("The number of results to search", "option", "n", int),
+    r=("The max number of results to display", "option", "r", int))
+def main(mandatory, optional, n, r):
+    if mandatory is None and optional is None:
+        sys.stderr.write("{}: At least one keyword (optional or mandatory) is required\n".format(sys.argv[0]))
+        sys.exit(1)
     reddit = praw.Reddit("askhist-search")
     results = search_with(reddit.subreddit("askhistorians"),
                           mandatory, optional=optional, n=n)
-    for link in results:
+    for link in results[:r]:
         print(link)
 
 
